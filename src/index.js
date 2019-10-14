7e9397cbfe3904307294b6f3d44afe01052169b0
Andy <russellandy98@gmail.com>
2019-10-14 17:59:21 -0400

    able to access my server and view posts section 2 completed

--- src/index.js
+++ src/index.js
@@ -10,14 +10,13 @@ import { Provider } from 'react-redux';
 //import App from './components/App';
 import PostsIndex from './containers/posts_index';
 import rootReducer from './reducers';
-
+import ReduxPromise from 'redux-promise';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import "./index.css";
 
 const compose = composeWithDevTools({trace:true})
 
-const store = createStore(rootReducer, {}, compose(applyMiddleware()));
-
+const store = createStore(rootReducer, {}, compose(applyMiddleware(ReduxPromise)));
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
